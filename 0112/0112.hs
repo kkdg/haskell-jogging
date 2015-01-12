9d691d0b5acd2ff3d27c7f4d6c4ef2674cd6db61
Kwag De Gi <code@skyaperture.com>
2015-01-12 09:37:13 +0900

    some intro

---
+++ 0112/0112.hs
@@ -0,0 +1,11 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100 then x else x*2
+
+doubleSmallNumber' x = ( if x > 100
+			then x
+			else x*2
+			) + 1
+
