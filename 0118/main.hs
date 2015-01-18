66f8beb0e5721f8522040f414eae15ca4d672667
Kwag De Gi <code@skyaperture.com>
2015-01-18 16:12:40 +0900

    list range done

--- 0118/main.hs
+++ 0118/main.hs
@@ -2,3 +2,44 @@
 doubleSmallNumber x = ( if x > 100 then x else x*2 ) + 1
 
 conanO'Brien = "It's a-me, Conan O'Brien!"
+
+
+a = [1,2,3,4] ++ [9,10,11,12]
+
+b = ['w','o'] ++ ['o','t']
+
+c = 'A':" Small CAT"
+
+d = 5:[1,2,3,4,5]
+
+e = "Degi Kwag" !! 3
+
+f = [3,2,1] > [2,1,3]
+
+g = head [1,2,3,45]
+
+g1 = [5,4,3,2,1]
+
+h = tail g1
+
+i = last g1
+
+j = init g1
+
+k = length g1
+
+l = null g1
+
+m = reverse g1
+
+n = take 2 g1
+
+o = drop 3 g1
+
+p = sum g1
+
+q = product [1,2,3,4,5]
+
+r = [2,4..20]
+
+
